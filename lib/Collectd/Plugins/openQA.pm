e5a338b2661536fecf4d297d353b14431b6721d5
Santiago Zarate <santiago.zarate@suse.com>
2018-10-25 02:07:04 +0200

    Style changes

--- lib/Collectd/Plugins/openQA.pm
+++ lib/Collectd/Plugins/openQA.pm
@@ -12,6 +12,7 @@ Collectd::Plugins::openQA - a collectd perl plugin for openQA systemd instances
 =cut
 
 =head1 SYNOPSIS
+
 This is a collectd plugin for checking status of openQA worker systemd services.
 In your collectd config:
     LoadPlugin perl
@@ -23,6 +24,7 @@ In your collectd config:
                 worker_instances 10
         </Plugin>
     </Plugin>
+
 =cut
 
 =head1 DESCRIPTION
@@ -32,6 +34,7 @@ Small collectd plugin written as a PoC.
 =head1 AUTHOR
 
 Santiago Zarate - L<https://foursixnine.io/>
+
 =cut
 
 # enable autoflush
@@ -57,7 +60,8 @@ C<$BaseDir/rrd/$hostname/$plugin_name/$database_$_type_instance_name.rrd>
 =cut
 
 sub read {
-	my $vl = { plugin => $_collection_name, type => $_datatype };
+    my $vl = { plugin => $_collection_name, type => $_datatype };
+
     for (my $i=1; $i < $configuration->{worker_instances}+1; $i++){
         my $cmd = 'systemctl is-active --quiet openqa-worker@'.$i; # 0 means everything fine, 3 service is inactive
         $vl->{'values'} = [ `$cmd; echo -n \$?` ]; # 0 means everything fine, 3 service is inactive
@@ -66,14 +70,14 @@ sub read {
         plugin_dispatch_values ($vl);
     }
 
-	return 1;
+    return 1;
 }
 
 sub config {
     my ($config) = @{ $_[0]->{children} };
     # we only want worker_instances and we know for now, it's there
     $configuration->{ $config->{key} } = pop @{ $config->{values} };
-    plugin_log(LOG_NOTICE, "Will write: ". dump($configuration)); # debug
+    plugin_log(LOG_NOTICE, "Configuration set". dump($configuration)); # debug
     return 1;
 }
 
